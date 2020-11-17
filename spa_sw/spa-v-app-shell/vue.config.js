4d9bde8613f73c0e5429569cc3a7cfd8be883b01
Dirk Ecker <dirkecker@gmail.com>
2020-11-17 13:55:13 +0100

    added config files for vue

---
+++ spa_sw/spa-v-app-shell/vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/spa_sw/spa-v-app-shell/dist/' : '/'
+}
---
+++ spa_sw/spa-v-sw/vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/spa_sw/spa-v-sw/dist/' : '/'
+}
