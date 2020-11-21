3c6629d551557918b5a379d5d06eb7f22b9568bb
Dirk Ecker <dirkecker@gmail.com>
2020-11-21 11:16:42 +0100

    refactoring

--- request_optimisation/spa/spa-app-shell/src/router/index.js
+++ request_optimisation/spa/spa-app-shell/src/router/index.js
@@ -11,12 +11,20 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/content1',
+    name: 'Content1',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "content1" */ '../views/Content1.vue')
+  },
+  {
+    path: '/content2',
+    name: 'Content2',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "content2" */ '../views/Content2.vue')
   }
 ]
 
---
+++ request_optimisation/spa/spa-app-shell/src/views/Content1.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Content 1</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content1',
+}
+</script>
+
