0a7995536b9ade3cac46ba058258120c97443a7f
Dirk Ecker <dirkecker@gmail.com>
2020-11-21 11:10:10 +0100

    refactoring

--- request_optimisation/spa/spa-v-app-shell/src/App.vue
+++ request_optimisation/spa/spa-v-app-shell/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/content1">Content 1</router-link>
+      <router-link to="/content2">Content 2</router-link>
     </div>
     <router-view/>
   </div>
--- request_optimisation/spa/spa-v-app-shell/src/views/Content2.vue
+++ request_optimisation/spa/spa-v-app-shell/src/views/Content2.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>Content 2</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: 'Content2',
 }
 </script>
+
